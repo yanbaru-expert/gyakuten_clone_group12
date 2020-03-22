0519d7f83049ade0c0055321721d3fe190eb1174
常原清隆 <tsuneharakiyotaka@tsuneharakiyotakanoMacBook-Air.local>
2020-03-22 10:02:52 +0900

    Merge conflict2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
   root to: 'movies#index'
 end
