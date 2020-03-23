798da26ea29d3a772184f99148394d22d7bf8c38
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-03-24 00:27:44 +0900

    fix stylesheet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,4 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import 'bootstrap'
-import '../src/application.scss'
+require("bootstrap")
