58909dec286b8b59685ddd04a7aa01c62a2c1556
常原清隆 <tsuneharakiyotaka@tsuneharakiyotakanoMacBook-Air.local>
2020-03-23 19:07:46 +0900

    5_task

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'movies#index'
