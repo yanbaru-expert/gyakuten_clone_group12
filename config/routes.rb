5a456e2e92734103fdb8df3a0443bc6fefb0cf36
山本龍太 <yamamotoryuuta@yamamotoryuutanoiMac.local>
2020-03-26 16:18:14 +0900

    ログイン関連のページの見た目を整える

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_controllerをカスタムするための記述
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+  
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+  end
+
+
+  
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'movies#index'
