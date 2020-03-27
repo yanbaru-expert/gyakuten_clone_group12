93dea69393848c8ca36843884b22a76bad9b68ee
山本龍太 <yamamotoryuuta@yamamotoryuutanoiMac.local>
2020-03-27 11:35:54 +0900

    8_task_yamaryuu_fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  # devise_controllerをカスタムするための記述
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
-  } 
-  
-  devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy" 
-  end
+  devise_for :users
 
-
-  
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'movies#index'
