60b1f9e1cab1dabe1ca61d482deed4fcf3801d10
keiji77 <57708959+keiji77@users.noreply.github.com>
2020-04-02 17:22:17 +0900

    コンフリクトの解消13

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,32 @@ ActiveRecord::Schema.define(version: 2020_03_28_065115) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
+  create_table "awstexts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_items_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_items_on_reset_password_token", unique: true
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "questions", force: :cascade do |t|
     t.text "title"
     t.text "detail"
--- lib/tasks/import_csv.rake
+++ lib/tasks/import_csv.rake
@@ -3,8 +3,10 @@ namespace :import_csv do
   task questions: :environment do
     Question.import('db/csv_data/question_data.csv')
   end
-
+  #rakeコマンド
+  #rake import_csv:aws
   desc "AWS CSVをインポート"
   task aws: :environment do
     Awstext.import('db/csv_data/aws_text_data.csv')
+  end
 end
